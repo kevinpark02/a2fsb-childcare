a30359c9e98fd3e02e50be2bb92fc58232f04e55
kevin park <kevin.park02@gmail.com>
2022-03-24 13:47:29 -0700

    Set up the passport

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+//! DECLARING CONSTANTS - START
+    // use this for passport that this is the strategy we want to use for handling json web tokens
+        const JwtStrategy = require("passport-jwt").Strategy;
+        const ExtractJwt = require('passport-jwt').ExtractJwt;
+    // grab the user model
+        const mongoose = require("mongoose");
+        const User = mongoose.model("users");
+        const keys = require("./keys");
+//! DECLARING CONSTANTS - END
+
+//! Passport Options - START
+    const options = {};
+    options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken;
+    options.secretOrKey =  keys.secretOrKey;
+//! Passport Options - END
+
+module.exports = passport => {
+    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+        console.log(jwt_payload);
+        done();
+    }));
+
+}
