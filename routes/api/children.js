96e87ae5c7f5f52433d56478c038175e871a7d63
kevin park <kevin.park02@gmail.com>
2022-03-25 15:25:35 -0700

    Add more routes to the children route

--- routes/api/children.js
+++ routes/api/children.js
@@ -2,13 +2,58 @@
     // Router object is returned using express
         const express = require("express");
         const router = express.Router();
+    // Passport
+        const passport = require('passport');
+    // Validation
+        const validateChildInput = require("../../validation/children");
+    // Child model
+        const Child = require("../../models/Child");
 //! DECLARING CONSTANTS - END
 
 //! ADD ROUTES - START
-    // Test route
-        router.get("/test", (req, res) => {
-            res.json({ msg: "This is the children route" });
-        });
+    // Test route - No need in the future
+        // router.get("/test", (req, res) => {
+        //     res.json({ msg: "This is the children route" });
+        // });
+
+    // Get route
+        router.get("/", (req, res) => {
+            Child
+                .find()
+                .sort({ date: -1 })
+                .then(children => res.json(children))
+                .catch(err => res.status(400).json(err))
+        })
+
+    //! Get children by parents - need to finish this up later
+        // router.get("/user/:user_id", (req, res) => {
+        //     Child
+        //         .find({ parent })
+        // })
+
+    // Post route
+        router.post("/",
+            passport.authenticate("jwt", { session: false }),
+            (req, res) => {
+                const { isValid, errors } = validateChildInput(req.body);
+
+                if(!isValid) {
+                    return res.status(400).json(errors);
+                }
+
+                const newChild = new Child({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    gender: req.body.gender,
+                    parents: req.user.id
+                });
+
+                newChild
+                    .save()
+                    .then(child => res.json(child));
+
+            }
+        )
 //! ADD ROUTES - END
 
 module.exports = router;
