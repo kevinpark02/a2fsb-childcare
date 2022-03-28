f7ed82fd14c34413e1daa61f6cb610af23cea484
kevin park <kevin.park02@gmail.com>
2022-03-28 10:59:42 -0700

    Fix routes

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -19,7 +19,7 @@ class NavBar extends React.Component {
     if (this.props.loggedIn) {
       return (
         <div>
-          <Link to={"/tweets"}>All Tweets</Link>
+          <Link to={"/children"}>All Tweets</Link>
           <Link to={"/profile"}>Profile</Link>
           <Link to={"/new_tweet"}>Write a Tweet</Link>
           <button onClick={this.logoutUser}>Logout</button>
