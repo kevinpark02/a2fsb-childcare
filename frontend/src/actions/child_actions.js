f1a50a52e7cca2b5de61c1962e03571dd8c56b6b
kevin park <kevin.park02@gmail.com>
2022-03-28 15:45:33 -0700

    Create children actions

---
+++ frontend/src/actions/child_actions.js
@@ -0,0 +1,38 @@
+import { getChildren, getUserChildren, createChild } from "../util/child_api_util";
+
+export const RECEIVE_CHILDREN = "RECEIVE_CHILDREN";
+export const RECEIVE_USER_CHILDREN = "RECEIVE_USER_CHILDREN";
+export const RECEIVE_NEW_CHILD = "RECEIVE_NEW_CHILD";
+
+export const receiveChildren = children => ({
+    type: RECEIVE_CHILDREN,
+    children
+});
+
+export const receiveUserChildren = children => ({
+    type: RECEIVE_USER_CHILDREN,
+    children
+});
+
+export const receiveNewChild = child => ({
+    type: RECEIVE_NEW_CHILD,
+    child
+});
+
+export const fetchChildren = () => dispatch => (
+    getChildren()
+        .then(children => dispatch(receiveChildren(children)))
+        .catch(err => console.log(err))
+);
+
+export const fetchUserChildren = id => dispatch => (
+    getUserChildren(id)
+        .then(children => dispatch(receiveUserChildren(children)))
+        .catch(err => console.log(err))
+);
+
+export const makeChild = data => dispatch => (
+    createChild(data)
+        .then(child => dispatch(receiveNewChild(child)))
+        .catch(err => console.log(err))
+);
