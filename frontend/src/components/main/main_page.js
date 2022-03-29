25813129c25c01cd7ff3845bf88829eaae13d01f
kevin park <kevin.park02@gmail.com>
2022-03-28 20:39:05 -0700

    Delete unnecessart texts

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,7 +5,6 @@ class MainPage extends React.Component {
     return (
       <div>
         <h1>a2f sb childcare</h1>
-        <footer>Copyright &copy; 2019 Chirper</footer>
       </div>
     );
   }
