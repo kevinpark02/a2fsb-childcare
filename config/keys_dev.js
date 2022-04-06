89ca2cd2d9e8045f6374497edb854cc58208de08
kevin park <kevin.park02@gmail.com>
2022-04-06 11:25:53 -0700

    Configure keys_prod

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin:CW6M2DAjHIaj2JRF@a2fsb.mmeyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "qkrrbgus0820@rlaguswls1961w/qkrdmstoa311",
+  registrationCode: "W&kUAKOg&2GeKr@wHh7%wGJSF^$l)7",
+};
