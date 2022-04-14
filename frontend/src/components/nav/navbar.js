993245ef5c28297c4b1f44858c97a85a0e11ec17
Kevin Park <kevin.park02@gmail.com>
2022-04-14 16:35:09 -0700

    Revert "Kp navbar design"

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendar, faBaby, faHand, faDoorOpen } from "@fortawesome/free-solid-svg-icons";
-
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -19,34 +16,13 @@ class NavBar extends React.Component {
 
   // Selectively render links dependent on whether the user is logged in
   getLinks() {
-    const calendarIcon = <FontAwesomeIcon icon={faCalendar} className="navbar-menu-link navbar-menu-icon"/>;
-    const childrenIcon = <FontAwesomeIcon icon={faBaby} className="navbar-menu-link navbar-menu-icon"/>;
-    const volunteersIcon = <FontAwesomeIcon icon={faHand} className="navbar-menu-link navbar-menu-icon"/>;
-    const logoutIcon = <FontAwesomeIcon icon={faDoorOpen} className="navbar-menu-link navbar-menu-icon"/>;
-
     if (this.props.loggedIn) {
       return (
-        <div className="navbar-container"> 
-          <div className="navbar-menu-link navbar-top"></div>
-          <div className="navbar-menu-link navbar-mid">
-            <Link to={"/calendar"} className="navbar-menu-link">
-              {calendarIcon}
-              calendar
-            </Link>
-            <Link to={"/children"} className="navbar-menu-link">
-              {childrenIcon}
-              children
-            </Link>
-            <Link to={"/volunteers"} className="navbar-menu-link">
-              {volunteersIcon}
-              volunteers
-            </Link>
-          </div>
-          <div className="navbar-menu-link navbar-bot">
-            <button onClick={this.logoutUser} className="navbar-menu-link">
-              {logoutIcon}log out
-            </button>
-          </div>         
+        <div>
+          <Link to={"/children"}>All Children</Link>
+          <Link to={"/profile"}>Profile</Link>
+          <Link to={"/new_child"}>Create Child</Link>
+          <button onClick={this.logoutUser}>Logout</button>
         </div>
       );
     }
@@ -54,7 +30,9 @@ class NavBar extends React.Component {
 
   render() {
     return (
-        this.getLinks()
+      <div>
+        {this.getLinks()}
+      </div>
     );
   }
 }
