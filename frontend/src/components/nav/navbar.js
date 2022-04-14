a7b49e549d9ec889bdbfac8702e2b04e9f0213f9
kevin park <kevin.park02@gmail.com>
2022-04-14 15:23:00 -0700

    Add icons next to text

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faBaby, faHand } from "@fortawesome/free-solid-svg-icons";
+
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -17,20 +19,23 @@ class NavBar extends React.Component {
 
   // Selectively render links dependent on whether the user is logged in
   getLinks() {
+    const calendarIcon = <FontAwesomeIcon icon={faCalendar} className="navbar-menu-link"/>;
+    const childrenIcon = <FontAwesomeIcon icon={faBaby} className="navbar-menu-link"/>;
+    const volunteersIcon = <FontAwesomeIcon icon={faHand} className="navbar-menu-link"/>;
+
     if (this.props.loggedIn) {
       return (
-        <div className="navbar-container">
-          <div>
-
-          </div>
-          <FontAwesomeIcon icon={solid("user-secret")} />
+        <div className="navbar-container">          
           <Link to={"/calendar"} className="navbar-menu-link">
+            {calendarIcon}
             calendar
           </Link>
           <Link to={"/children"} className="navbar-menu-link">
+            {childrenIcon}
             children
           </Link>
           <Link to={"/volunteers"} className="navbar-menu-link">
+            {volunteersIcon}
             volunteers
           </Link>
           <button onClick={this.logoutUser}>Logout</button>
