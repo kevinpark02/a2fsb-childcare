023b7bd4e98d32daa926ab5b7076dcaa7876c8ca
kevin park <kevin.park02@gmail.com>
2022-04-16 07:58:25 -0700

    configure modal reducer

--- frontend/src/actions/modal_actions.js
+++ frontend/src/actions/modal_actions.js
@@ -1,11 +1,11 @@
 export const OPEN_MODAL = "OPEN_MODAL";
 export const CLOSE_MODAL = "CLOSE_MODAL";
 
-export const openModal = (modal, cardId) => {
+export const openModal = (modal, childId) => {
   return {
     type: OPEN_MODAL,
     modal,
-    cardId,
+    childId,
   };
 };
 
---
+++ frontend/src/reducers/modal_reducer.js
@@ -0,0 +1,12 @@
+import { OPEN_MODAL, CLOSE_MODAL } from "../actions/modal_actions";
+
+export default function modalReducer(state = [], action) {
+  switch (action.type) {
+    case OPEN_MODAL:
+      return [action.modal, action.cardId];
+    case CLOSE_MODAL:
+      return [];
+    default:
+      return state;
+  }
+}
