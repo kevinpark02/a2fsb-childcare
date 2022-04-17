4de73eddc45c65d50104f7f9f9c13bcf266b068a
kevin park <kevin.park02@gmail.com>
2022-04-17 08:06:23 -0700

    Have the children page fetch all children after making a new child

--- frontend/src/components/children/new_child_container.js
+++ frontend/src/components/children/new_child_container.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { makeChild } from '../../actions/child_actions';
+import { fetchChildren, makeChild } from '../../actions/child_actions';
 import CreateChild from './create_child';
 
 const mapStateToProps = (state) => {
@@ -12,6 +12,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => {
     return {
         makeChild: data => dispatch(makeChild(data)),
+        fetchChildren: () => dispatch(fetchChildren()),
     };
 };
 
