8765f0477790e02a23bc1e9d6cf2202faeb14780
kevin park <kevin.park02@gmail.com>
2022-04-18 23:51:45 -0700

    Add birthday attribute to the child model

--- models/Child.js
+++ models/Child.js
@@ -14,6 +14,10 @@ const ChildSchema = new Schema({
         type: String,
         required: true,
     },
+    birthday: {
+        type: Date,
+        required: true,
+    },
     parents: [{
         type: Schema.Types.ObjectId,
         ref: 'users',
