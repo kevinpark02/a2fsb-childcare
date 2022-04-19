e2b2c7e2210b0b739ba55c3556fdc75fe0a7f7e2
kevin park <kevin.park02@gmail.com>
2022-04-19 00:47:43 -0700

    Create child error reducer

--- frontend/src/actions/child_actions.js
+++ frontend/src/actions/child_actions.js
@@ -5,6 +5,7 @@ export const RECEIVE_USER_CHILDREN = "RECEIVE_USER_CHILDREN";
 export const RECEIVE_NEW_CHILD = "RECEIVE_NEW_CHILD";
 export const CLEAR_CHILD = "CLEAR_CHILD";
 export const EDIT_CHILD = "EDIT_CHILD";
+export const RECEIVE_CHILD_ERRORS = "RECEIVE_CHILD_ERRORS";
 
 export const receiveChildren = children => ({
     type: RECEIVE_CHILDREN,
@@ -29,6 +30,11 @@ export const alterChild = child => ({
 export const clearChild = childId => ({
     type: CLEAR_CHILD,
     childId
+});
+
+export const receiveErrors = (errors) => ({
+    type: RECEIVE_CHILD_ERRORS,
+    errors,
 })
 
 export const fetchChildren = () => dispatch => (
@@ -46,7 +52,7 @@ export const fetchUserChildren = id => dispatch => (
 export const makeChild = data => dispatch => (
     createChild(data)
         .then(child => dispatch(receiveNewChild(child)))
-        .catch(err => console.log(err))
+        .catch(err => dispatch(receiveErrors(err.response.data)))
 );
 
 export const removeChild = childId => dispatch => (
