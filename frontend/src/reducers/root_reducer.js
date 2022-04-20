887d4f70e4eed490cae632f1886468b24c4edc47
kevin park <kevin.park02@gmail.com>
2022-04-19 23:54:45 -0700

    Include volunteers to the root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,12 +2,14 @@ import { combineReducers } from "redux";
 import session from "./session_reducer";
 import error from "./errors_reducer";
 import children from "./children_reducer";
+import volunteers from "./volunteers_reducer";
 import ui from "./ui_reducer";
 
 const RootReducer = combineReducers({
   session,
   error,
   children,
+  volunteers,
   ui,
 });
 
