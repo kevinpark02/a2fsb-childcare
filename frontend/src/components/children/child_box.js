868c1ebc3141f06f65494164dbe964cb87da5094
wesley <cheung.w.2016@gmail.com>
2022-05-04 14:49:19 -0700

    fixed some css on children main page

--- frontend/src/components/children/child_box.js
+++ frontend/src/components/children/child_box.js
@@ -22,21 +22,14 @@ class ChildBox extends React.Component {
         }
         let profilePic;
         let name = this.props.child.firstName + " " + this.props.child.lastName
-        console.log(name)
         if(!child.photoUrl){
-            // AvatarGenerator.generate({ name: "Ameer Jhan", size: 64 }).then(avatar => {
-            // profilePic = AvatarGenerator.writeAvatar(avatar, "./default-avatar.jpg");
-            // });
-            // profilePic = <Avatar facebookId="100008343750912" size="100" />
-            profilePic = <Avatar fgColor="#F7F0DE" round="true" name={name} />
+            profilePic = <Avatar className="child-profile-pic-default" fgColor="#F7F0DE" round="true" name={name} />
         } else {
             profilePic = <img className="child-profile-pic" src={`api/images/${child.photoUrl}`}></img>
         }
         return (
           <Link onClick={this.handleClick}>
             <div className="child-link">
-              {/* <div className="child-temp-pic"></div> */}
-              {/* <img className="child-profile-pic" src={profilePic}></img> */}
               {profilePic}
               <button className="children-index">
                 <p>{this.props.firstName}</p>
