76338ff211f8b80063168549d4efe4487d952b45
kevin park <kevin.park02@gmail.com>
2022-05-12 16:32:20 -0700

    Update the errors root reducer

--- frontend/src/reducers/errors_reducer.js
+++ frontend/src/reducers/errors_reducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 
 import SessionErrorsReducer from "./session_errors_reducer";
 import ChildErrorsReducer from "./child_errors_reducer";
-import EventErrorsReducer from "./event_errors_reducer"''
+import EventErrorsReducer from "./event_errors_reducer";
 
 export default combineReducers({
   session: SessionErrorsReducer,
